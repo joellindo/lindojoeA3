6a3caea1d447ab37f8a74f421c01232b4e12bc4c
joellindo <52580005+joellindo@users.noreply.github.com>
2019-07-26 20:28:31 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,8 @@
+import {SongInfo} from '../../app/songInfo';
+
+export const SONGS : SongInfo[] = [
+    {name: 'How Far ill go', artist: 'Alessia Cara', genre: 'Soundtracks', yearRelease: '2016', picture: '../assets/images/moana.jpg'},
+    {name: 'A Whole New World', artist: 'Lea Salonga & Brad Kane', genre: 'Soundtracks', yearRelease: '1992', picture: '../assets/images/aladdin.jpg'},
+    {name: 'Let It Go', artist: 'Idina Menzel', genre: 'Soundtracks', yearRelease: '2013', picture: '../assets/images/frozen.jpg'},
+    {name: 'Circle of Life', artist: 'Carmen Twillie & Lebo M.', genre: 'Soundtracks', yearRelease: '1994', picture: '../assets/images/lionking.jpg'}
+]
---
+++ assets/data/student.ts
@@ -0,0 +1,3 @@
+import {lindojoe} from '../../app/lindojoe';
+
+export const student : lindojoe = {sid: '991492183', sname: 'Joel Lindo', scampus: 'Trafalger', slogin: 'lindojoe', assignment: '3'}
